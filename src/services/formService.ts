59a853b40fa73e31e287b156c9b0dad5fbf4dfcb
Nicolas Braga <nicolas.almeida@gsd.com.br>
2023-10-31 17:59:51 -0300

    tipando type em formService

--- src/services/formService.ts
+++ src/services/formService.ts
@@ -4,17 +4,17 @@ type Task = {
   isCompleted: boolean
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 // não consegui tipar o payload corretamente
-export function reducer(state: Task[], action: { type: any; payload: any }) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function reducer(state: Task[], action: { type: string; payload: any }) {
   switch (action.type) {
     case 'add-task': {
       return [
         ...state,
         {
           id: Date.now(), // Gera um ID único com base na data atual
-          value: action.payload,  // O valor da nova tarefa 
-          isCompleted: false,  // A tarefa é inicialmente definida como não concluída
+          value: action.payload, // O valor da nova tarefa
+          isCompleted: false, // A tarefa é inicialmente definida como não concluída
         },
       ]
     }
@@ -24,7 +24,7 @@ export function reducer(state: Task[], action: { type: any; payload: any }) {
     }
 
     default: {
-      //Em caso de uma ação inesperada retorna o estado
+      // Em caso de uma ação inesperada retorna o estado
       return state
     }
   }
